5b12cf76a50f7cfdd70d282691c09136fc5f865f
Sacha Bron <sacha.bron@copperflake.com>
2016-02-07 16:40:32 +0100

    Init Tag Manager and Process Manager

--- web-manager/router.js
+++ web-manager/router.js
@@ -1,3 +1,15 @@
 Router.route('/', function () {
 	this.render('manualMode');
 });
+
+Router.route('/tags', function () {
+	this.render('tagManager');
+});
+
+Router.route('/processes', function () {
+	this.render('processes');
+});
+
+Router.route('/manual', function () {
+	this.render('manualMode');
+});
