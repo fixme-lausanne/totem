03d47750406e6d001d1ec95149a653aa49cbdf41
Sacha Bron <sacha.bron@copperflake.com>
2016-02-09 04:39:22 +0100

    Add process launcher and filter according to tags. It works!

---
+++ web-manager/process-launcher.js
@@ -0,0 +1,60 @@
+if (Meteor.isServer) {
+	let runningProcesses = {};
+
+	Meteor.methods({
+		launchProcess: function (id, command) {
+			console.log('lauching ' + command);
+			const spawn = Npm.require('child_process').spawn;
+
+			if (runningProcesses[id]) {
+				console.log("This process is already running.");
+				return;
+			}
+
+			commandArr = command.split(' ');
+			const program = commandArr.shift();
+			const params = commandArr;
+
+			try {
+				let proc = spawn(program, params);
+				runningProcesses[id] = proc;
+				
+				proc.stdout.on('data', (data) => {
+					try {
+						const msg = JSON.parse(data.toString());
+						console.dir(msg);
+
+						/*Meteor.bindEnvironment(function () {
+							msg.screens = Screens.find({ tags: { $in: ["appliances", "school"] } }, { number: 1 }).map(screen => screen.number);
+
+							console.dir(msg.screens);
+
+							// emitMsg(msg);
+						});*/
+
+						var Fiber = Npm.require('fibers');
+
+						Fiber(function() {
+							// Magic
+							msg.screens = Screens.find({ tags: { $elemMatch: { value: { $in: msg.tags } } } }, { number: 1 }).map(screen => screen.number);
+							Meteor.call('emitMsg', msg);
+						}).run();
+					} catch (e) {
+						console.warn('Bad data:', e)
+					}
+				});
+
+				proc.stderr.on('data', (data) => {
+					console.error(`stderr (${command}): ${data}`);
+				});
+
+				proc.on('close', (code) => {
+					console.log(`child process exited with code ${code}`);
+					// TODO remove from runningProcesses
+				});
+			} catch (e) {
+				console.error(`Command ${command} failed: ${e}`);
+			}
+		}
+	});
+}
--- web-manager/web-manager.js
+++ web-manager/web-manager.js
@@ -1,5 +1,8 @@
 if (Meteor.isServer) {
 	Meteor.startup(function () {
-		// code to run on server at startup
+		// Launch processes
+		let procs = Processes.find({ enable: true }).forEach((proc) => {
+			Meteor.call('launchProcess', proc._id, proc.command);
+		});
 	});
 }
